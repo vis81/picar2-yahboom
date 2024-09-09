dab3b7e83a1078bbe0234d602639691bc78aa8a0
Valentyn Shevchenko <vis81@mail.ru>
2024-09-09 00:17:03 -0700

    yahboom: add control interface via USB uart

--- yahboom/src/main.c
+++ yahboom/src/main.c
@@ -15,6 +15,7 @@
 #include <zephyr/drivers/pwm.h>
 #include <zephyr/drivers/rc.h>
 #include <zephyr/drivers/uart.h>
+#include <zephyr/drivers/uart_pipe.h>
 #include "zephyr/drivers/pwm_servo.h"
 #include "zephyr/drivers/motor.h"
 #include <zephyr/logging/log.h>
@@ -27,11 +28,13 @@
 
 #include "battery.h"
 #include "buzzer.h"
+#include "control.h"
 #include "motor.h"
 #include "rc.h"
 #include "servo.h"
 
-//LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
 
 
 int main(void)
@@ -42,55 +45,9 @@ int main(void)
 	motor_init();
 	servo_init();
 	rc_init();
+	control_init();
 
 	//buzzer_play(BUZZER_FUNKYTOWN, 50);
+
+	return 0;
 }
-
-#ifdef CONFIG_SHELL
-
-static int cmd_m(const struct shell *sh, size_t argc,
-			      char **argv)
-{
-	int32_t vel[MOTOR_CNT];
-	int ret = 0;
-
-	if ( argc != 3
-		|| sscanf(argv[1],"%d", &vel[0]) != 1
-		|| sscanf(argv[1],"%d", &vel[1]) != 1
-		) {
-		ret = -EINVAL;
-		goto exit;
-	}
-	ret = motor_speed(MOTOR_L, vel[0]);
-	ret |= motor_speed(MOTOR_R, vel[1]);
-exit:
-	shell_print(sh,"a:%d", ret);
-	return ret;
-}
-
-static int cmd_p(const struct shell *sh, size_t argc,
-			      char **argv)
-{
-	int32_t pos[MOTOR_CNT];
-	int ret = 0;
-
-	if ( argc != 1) {
-		ret = -EINVAL;
-		goto exit;
-	}
-	ret = motor_pos(MOTOR_L, &pos[0]);
-	ret |= motor_pos(MOTOR_R, &pos[1]);
-exit:
-	shell_print(sh,"a:%d:%d,%d", ret, pos[0], pos[1]);
-	return ret;
-}
-
-SHELL_STATIC_SUBCMD_SET_CREATE(sub_c,
-	SHELL_CMD(m, NULL, "", cmd_m),
-	SHELL_CMD(p, NULL, "", cmd_p),
-	SHELL_SUBCMD_SET_END /* Array terminated. */
-);
-
-SHELL_CMD_REGISTER(c, &sub_c, "API commands", NULL);
-
-#endif
