5d1181b933da52b221083bf0cdf6979f5ba74131
Valentyn Shevchenko <vshevchenko@meta.com>
2026-05-08 18:44:12 -0700

    boards: yahboom: auto-start application after stm32flash upload
    Add --execution-addr=0x08000000 to the stm32flash runner args so the
    board resets and begins executing the new firmware immediately after
    flashing, eliminating the need for a manual reset.

--- boards/arm/yahboom_ros_stm32f103/board.cmake
+++ boards/arm/yahboom_ros_stm32f103/board.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--device=STM32F103RC" "--speed=4000")
-board_runner_args(stm32flash "--baud-rate=115200" "--start-addr=0x08000000")
+board_runner_args(stm32flash "--baud-rate=115200" "--start-addr=0x08000000" "--execution-addr=0x08000000")
 
 include(${ZEPHYR_BASE}/boards/common/stm32flash.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
